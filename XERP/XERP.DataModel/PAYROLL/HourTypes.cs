83ba22ceead0844231b814b97a7b418a7090a4cb
Raymart030493 <raymart04entuna@gmail.com>
2019-07-13 23:09:53 +0800

    ***Create Payroll Models ***Add new Regions models under HR folder.

---
+++ XERP/XERP.DataModel/PAYROLL/HourTypes.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XERP.DataModel.PAYROLL
+{
+    public class HourTypes {
+        [Required]
+        public Guid ID { get; set; }
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public bool active { get; set; }
+    }
+}
